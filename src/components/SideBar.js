1b354d657dd74e9601ce19f56e80cc09fd393bfc
Sour64 <digitalsorrow64@gmail.com>
2025-02-28 15:58:10 +0300

    selected day

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,12 +3,23 @@ import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 const SideBar = ({ days, addDay, deleteDay}) => {
+    const [selectedDay, setSelectedDay] = useState(null);
     const [dayName, setDayName] = useState('');
 
     const handleAddDay = () => {
         if (dayName.trim()) {
             addDay({ id: uuidv4(), name: dayName, tasks: [] });
             setDayName('');
+        }
+    };
+
+    const selectDay = (day) => {
+        setSelectedDay(day)
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleAddDay();
         }
     };
 
@@ -20,13 +31,26 @@ const SideBar = ({ days, addDay, deleteDay}) => {
                 placeholder="day name"
                 value={dayName}
                 onChange={(e) => setDayName(e.target.value)}
+                onKeyDown={handleKeyDown}
             />
-            <button className={"but_side"} onClick={handleAddDay}>add a day</button>
+            <button className={"add_day_button"} onClick={handleAddDay}>add a day</button>
             <ul className={"ul_side"}>
                 {days.map((day) => (
-                    <li className={"li_side"} key={day.id}>
+                    <li
+                        className={`li_side ${selectedDay?.id === day.id ? 'selected' : ''}`}
+                        key={day.id}
+                        onClick={()=> selectDay(day)}
+                    >
                         <Link to={`/day/${day.id}`}>{day.name}</Link>
-                        <button className={"delete_but"} onClick={() => deleteDay(day.id)}>delete</button>
+                        <button
+                            className="delete_but"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                deleteDay(day.id);
+                            }}
+                        >
+                            delete
+                        </button>
                     </li>
                 ))}
             </ul>
